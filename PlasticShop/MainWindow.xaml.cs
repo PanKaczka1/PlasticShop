626e772ac81d70e308d319848ee2f8c9dc414648
Kacper Niescierowicz <kapoludek@gmail.com>
2020-12-04 15:00:36 +0100

    Revert "e"
    This reverts commit bdc6e46164ea6209e2db05b29a8be191f2cad8c3.

--- PlasticShop/MainWindow.xaml.cs
+++ PlasticShop/MainWindow.xaml.cs
@@ -31,7 +31,6 @@ namespace PlasticShop
         ObservableCollection<INFOORDERCUSTOMER> customerOrders;
         ObservableCollection<INFOSTOREORDER> storeOrders;
         ObservableCollection<PRODUCT> products;
-        ObservableCollection<CUSTOMERORDER> productsInOrder;
 
         public MainWindow()
         {
@@ -44,7 +43,6 @@ namespace PlasticShop
             customerOrders = new ObservableCollection<INFOORDERCUSTOMER>();
             storeOrders = new ObservableCollection<INFOSTOREORDER>();
             products = new ObservableCollection<PRODUCT>();
-            productsInOrder = new ObservableCollection<CUSTOMERORDER>();
             using (var context = new Entities())
             {
                 foreach (var pencil in context.PENCILS)
@@ -688,8 +686,8 @@ namespace PlasticShop
         {
             var customerOrder = new INFOORDERCUSTOMER();
             var storeOrder = new INFOSTOREORDER();
-            var cOrderList = new List<CUSTOMERORDER>();
-            var sOrderList = new List<STOREORDER>();
+            var cOrder = new CUSTOMERORDER();
+            var sOrder = new STOREORDER();
             using (var context = new Entities())
             {
                 if (context.INFOORDERCUSTOMERs.Any())
@@ -697,11 +695,8 @@ namespace PlasticShop
                     decimal id = context.INFOORDERCUSTOMERs.Max(p => p.ORDER_ID);
                     customerOrder.ORDER_ID = id + 1;
                     storeOrder.ORDER_ID = id + 1;
-                }
-                foreach(var item in productsInOrder)
-                {
-                    cOrderList.Add(new CUSTOMERORDER() { NUMBER_OF_PRODUCTS = item.NUMBER_OF_PRODUCTS, PRODUCT_ID = item.PRODUCT_ID, ORDER_ID = customerOrder.ORDER_ID });
-                    sOrderList.Add(new STOREORDER() { NUMBER_OF_PRODUCTS = item.NUMBER_OF_PRODUCTS, PRODUCT_ID = item.PRODUCT_ID, ORDER_ID = customerOrder.ORDER_ID });
+                    cOrder.ORDER_ID = id + 1;
+                    sOrder.ORDER_ID = id + 1;
                 }
             }
             customerOrder.ORDER_DATE = (System.DateTime)orderDate.SelectedDate;
@@ -722,17 +717,8 @@ namespace PlasticShop
             {
                 context.INFOORDERCUSTOMERs.Add(customerOrder);
                 context.INFOSTOREORDERs.Add(storeOrder);
-                foreach(var itemC in cOrderList)
-                {
-                    context.CUSTOMERORDERS.Add(itemC);
-                }
-                foreach(var itemS in sOrderList)
-                {
-                    context.STOREORDERS.Add(itemS);
-                }
                 var item = context.INFOORDERCUSTOMERs.Find(customerOrder.ORDER_ID);
                 customerOrders.Add(new INFOORDERCUSTOMER() { ORDER_DATE = item.ORDER_DATE, ORDER_ID = item.ORDER_ID });
-                productsInOrder.Clear();
                 context.SaveChanges();
             }
         }
@@ -756,24 +742,6 @@ namespace PlasticShop
             {
                 products.Clear();
             }
-        }
-
-        private void addNewProduct_Click(object sender, RoutedEventArgs e)
-        {
-            try
-            {
-                productsInOrder.Add(new CUSTOMERORDER() { PRODUCT_ID = ((PRODUCT)productsOrder.SelectedItem).PRODUCT_ID, NUMBER_OF_PRODUCTS = decimal.Parse(productsAmount.Text) });
-            }
-            catch(Exception exc)
-            {
-                MessageBox.Show("Invalid data");
-            }
-            using(var context = new Entities())
-            {
-                var item = context.PRODUCTS.Find(((PRODUCT)productsOrder.SelectedItem).PRODUCT_ID);
-                MessageBox.Show("Product " + item.PRODUCT_NAME + " was added to order");
-            }
-            products.Clear();
         }
     }
 }
